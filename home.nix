af7667db4492f8db4f5f5a9452a5e95694e34b2a
Jason Reed <jcreed@gmail.com>
2024-05-01 11:02:07 -0400

    Add some dotfiles
    Resolves #4.

--- home.nix
+++ home.nix
@@ -21,7 +21,7 @@
     hello
     dconf # this is crucial for actually applying gnome-terminal settings below!
           # see https://github.com/nix-community/home-manager/issues/3113
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
@@ -30,11 +30,11 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
+    ".bashrc".source = dotfiles/bashrc;
+    ".gitconfig".source = dotfiles/gitconfig;
+    ".git-prompt.bash".source = dotfiles/git-prompt.bash;
+    ".git-completion.bash".source = dotfiles/git-completion.bash;
+    
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
